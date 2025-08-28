827191161119dcbe187760e2679ab9bab6cfb8ef
Stella <deloreyste@gmail.com>
2025-08-28 04:55:37 -0400

    commented general outline

--- src/main.py
+++ src/main.py
@@ -1,12 +1,40 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
 data = pd.read_csv("fashion-mnist_train.csv")
 test = pd.read_csv("fashion-mnist_test.csv")
 
+data = np.array(data)
+m, n = data.shape
+np.random.shuffle(data)
+
 def main():
     pass  
+
+## init params - weights, biases
+
+
+# linear transformation - result denoted Z
+# Z = sum(w * x) + b
+
+
+## forward propogation
+# layer 1 - ReLU
+
+# layer 2 - Softmax
+
+
+## backward propogation
+# loss calculation
+
+# gradient calculation
+
+# weight, bias update
+
+
+## gradient descent - iteration i times
+
 
 if __name__ == "__main__":
     main()
